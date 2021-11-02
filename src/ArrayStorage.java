26e00ab27f054128455488f409cef3c0569c1baa
ivanborodkin1997 <boroda0360@mail.ru>
2021-11-03 01:25:37 +0300

    Added methods in ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -9,11 +9,7 @@ public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
-        for (int i = 0; i < storage.length; i++) {
-            if (storage[i] != null) {
-                storage[i] = null;
-            }
-        }
+        Arrays.fill(storage,null);
     }
 
     void save(Resume resume) {
