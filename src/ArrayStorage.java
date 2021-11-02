dfc60919e206838738db9c8e307e2d9ce004b5b1
ivanborodkin1997 <boroda0360@mail.ru>
2021-11-03 01:21:28 +0300

    Added methods in ArrayStorage class

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,78 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 /**
  * Array based storage for Resumes
  */
+
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
 
     void clear() {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] != null) {
+                storage[i] = null;
+            }
+        }
     }
 
-    void save(Resume r) {
+    void save(Resume resume) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                storage[i] = resume;
+                break;
+            }
+        }
     }
 
     Resume get(String uuid) {
+        for (Resume resume : storage) {
+            if (resume.uuid.equals(uuid)) {
+                return resume;
+            }
+        }
         return null;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+                break;
+            }
+        }
+
+        for (int i = 0; i < storage.length; i++) {
+            if (storage[i] == null) {
+                for (int k = i + 1; k < storage.length; k++) {
+                    storage[k - 1] = storage[k];
+                }
+                storage[storage.length - 1] = null;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                count++;
+            }
+        }
+        return Arrays.copyOf(storage, count);
     }
 
     int size() {
-        return 0;
+        int count = 0;
+        for (Resume resume : storage) {
+            if (resume != null) {
+                count++;
+            }
+        }
+        return count;
     }
 }
